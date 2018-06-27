2636317b22c063feaf1a82d258861499481d2a83
ipatch <chris.r.jones.1983@gmail.com>
2018-06-26 20:19:57 -0500

    You are a very redundant person, that's what kind of person you are.

--- main.go
+++ main.go
@@ -1,11 +1,40 @@
 package main
 
 import (
+
+  "fmt"
+  "os"
+
 	"github.com/docker/machine/libmachine/drivers/plugin"
 	// "github.com/jigtools/docker-machine-driver-qemu"
   "github.com/ipatch/docker-machine-driver-qemu/qemu"
 )
 
+// func main() {
+// 	plugin.RegisterDriver(qemu.NewDriver("default", "path"))
+// }
+
 func main() {
-	plugin.RegisterDriver(qemu.NewDriver("default", "path"))
+  if len(os.Args) >= 2 && os.Args[1] == "qemu" {
+    runQemu()
+  } else {
+    ssh.SetDefaultClient(ssh.Native)
+    plugin.RegisterDriver(qemu.NewDriver("", ""))
+  }
+}
+
+func run Qemu() {
+  done := make(chan bool)
+  ptyCh := make(chan string)
+
+  args := os.Args[1:]
+
+  if os.Args[len(os.Args)-1] != "-M" {
+		fmt.Printf("Waiting on a pseudo-terminal to be ready... ")
+		pty := <-ptyCh
+		fmt.Printf("done\n")
+		fmt.Printf("Hook up your terminal emulator to %s in order to connect to your VM\n", pty)
+	}
+
+	<-done
 }
