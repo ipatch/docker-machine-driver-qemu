81b7d7ac11c941590e27ed83f95f46a9901d7557
ipatch <chris.r.jones.1983@gmail.com>
2018-06-27 10:48:23 -0500

    Familiarity breeds contempt -- and children. 		-- Mark Twain

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
   // NOTE: I changed the package source path to my local fork,
   // ...there's probably a better way to handle this.
   //
-  "github.com/ipatch/docker-machine-driver-qemu"
+  "github.com/ipatch/docker-machine-driver-qemu/cmd"
 	// "github.com/jigtools/docker-machine-driver-qemu"
 )
 
